26d0204710d110441111b4524951acabce4ebc5f
Gašper Katrašnik <gkatrasnik@gmail.com>
2023-09-23 17:08:57 +0200

    locale switcher created
    locale switcher created

---
+++ components/localeSwitcher.js
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+import {useRouter} from 'next/router';
+import {useTranslations} from 'next-intl';
+import { Dropdown } from 'react-bootstrap';
+import { useState } from 'react';
+
+export default function LocaleSwitcher() {
+  const t = useTranslations('Locales');
+  const {locale, locales, route} = useRouter();
+  const [dropdownShowed, setDropdownShowed] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownShowed(!dropdownShowed);
+  }
+  
+  return (   
+     <Dropdown show={dropdownShowed}>
+      <Dropdown.Toggle variant="" id="dropdown-basic" onClick={toggleDropdown}>
+        {locale}
+      </Dropdown.Toggle>      
+
+      <Dropdown.Menu  align='start'>
+        {locales.map(locale => {
+            return (   
+                <Dropdown.ItemText key={locale} onClick={toggleDropdown}>
+                    <Link  href={route} locale={locale}>
+                    {t("localeName", {locale: locale})}
+                    </Link>
+                </Dropdown.ItemText>      
+            ) 
+        })}
+
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
--- components/navbar.js
+++ components/navbar.js
@@ -1,17 +1,21 @@
 import { Navbar, Nav } from 'react-bootstrap'
 import { useState } from 'react'
 import Link from 'next/link'
+import LocaleSwitcher from './localeSwitcher';
 
 export default function MyNavbar() {
   const [expanded, setExpanded] = useState(false);
 
   return (
     <Navbar bg="light" expand="md" className="px-3" expanded={expanded}>
+      <div className='d-flex'>
       <Navbar.Brand>
         <Link href="/" passHref onClick={() => setExpanded(false)} >
             Ab Bike
         </Link>
       </Navbar.Brand>
+      <LocaleSwitcher/>
+      </div>
       <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(expanded => !expanded)}/>
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ms-auto">
@@ -23,7 +27,7 @@ export default function MyNavbar() {
           </Link>
           <Link  className='nav-link navbar-nav' href="/pricing" passHref onClick={() => setExpanded(false)}>
             Pricing
-          </Link>              
+          </Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
