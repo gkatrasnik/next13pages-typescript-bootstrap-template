b50a5711a42fce115c530bf0bebdf944a97f25b7
Gašper Katrašnik <72498702+gkatrasnik@users.noreply.github.com>
2023-09-24 17:05:17 +0000

    footer with links created

--- components/footer.js
+++ components/footer.js
@@ -1,11 +1,39 @@
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
+import {useTranslations} from 'next-intl';
+
 
 export default function MyFooter() {
-  return (
-    <Container fluid className="bg-light py-3">
-      <Container>
-        <p className="text-center mb-0">© 2023 AB Bike. All rights reserved.</p>
-      </Container>
+  const t = useTranslations('Footer');
+
+  return (    
+    <footer className='bg-light'>
+      <Container  className='px-4'>
+        <Row className='h-100'>         
+          <Col xs={12} md={{span:6, order:2}} className='py-4'>
+            <h5 className="fw-bold pb-1">{t("contact")}</h5>
+            <div className='d-flex flex-column'>              
+              <div className="fw-bold pb-1">
+                {t("tel")}
+                <a href="tel: +386 (0)40 219 415" className='footer-link'>+386 (0)40 219 415</a> 
+              </div>                      
+              <div className="fw-bold pb-1">
+                {t("email")}  
+                <a href="mailto:info@ab-bike.si" className='footer-link'>info@ab-bike.si</a>
+              </div> 
+              <div className="fw-bold pb-1">{t("address")}</div> 
+              <div className="fw-bold pb-1">{t("company")}</div> 
+            </div>
+          </Col>
+
+          <Col xs={12} md={{span:6, order:1}} className='py-4'>
+            <h5 className="fw-bold pb-1">{t("partners")}</h5>
+            <div className="fw-bold pb-1">LOGO</div> 
+            <div className="fw-bold pb-1">LOGO</div> 
+            <div className="fw-bold pb-1">LOGO</div> 
+            <div className="fw-bold pb-1">LOGO</div>
+          </Col>
+        </Row>      
     </Container>
+  </footer>
   )
 }
