f17d4565874465436c4177bf4e191996b97ef7e5
Gašper Katrašnik <72498702+gkatrasnik@users.noreply.github.com>
2023-09-24 16:01:56 +0000

    navbar bold

--- components/navbar.js
+++ components/navbar.js
@@ -4,7 +4,6 @@ import Link from 'next/link'
 import LocaleSwitcher from './localeSwitcher';
 import {useTranslations} from 'next-intl';
 
-
 export default function MyNavbar() {
   const t = useTranslations('Navbar');
   const [expanded, setExpanded] = useState(false);
@@ -13,7 +12,7 @@ export default function MyNavbar() {
   }
 
   return (
-    <Navbar bg="light" expand="lg" className="px-3" expanded={expanded}>
+    <Navbar bg="light" expand="lg" className="px-2" expanded={expanded}>
               <div className='d-flex'>
         <Navbar.Brand>
           <Link href="/" passHref onClick={() => setExpanded(false)} >
@@ -25,28 +24,28 @@ export default function MyNavbar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(expanded => !expanded)}/>
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ms-auto">
-          <Link  className='nav-link navbar-nav' href="/" passHref onClick={collapseNavbar}>
+          <Link  className='nav-link navbar-nav custom-nav-link' href="/" passHref onClick={collapseNavbar}>
             {t('home')}
           </Link>
-          <Link  className='nav-link navbar-nav' href="/rent-a-bike" passHref onClick={collapseNavbar}>
+          <Link  className='nav-link navbar-nav custom-nav-link' href="/rent-a-bike" passHref onClick={collapseNavbar}>
             {t('rentABike')}
           </Link>
-          <Link  className='nav-link navbar-nav' href="/rent-a-sup" passHref onClick={collapseNavbar}>
+          <Link  className='nav-link navbar-nav custom-nav-link' href="/rent-a-sup" passHref onClick={collapseNavbar}>
             {t('rentASup')}
           </Link>
-          <Link  className='nav-link navbar-nav' href="/cycling-trips" passHref onClick={collapseNavbar}>
+          <Link  className='nav-link navbar-nav custom-nav-link' href="/cycling-trips" passHref onClick={collapseNavbar}>
             {t('cyclingTrips')}
           </Link>
-          <Link  className='nav-link navbar-nav' href="/bike-servis" passHref onClick={collapseNavbar}>
+          <Link  className='nav-link navbar-nav custom-nav-link' href="/bike-servis" passHref onClick={collapseNavbar}>
             {t('bikeServis')}
           </Link>
-          <Link  className='nav-link navbar-nav' href="/ebike-upgrade" passHref onClick={collapseNavbar}>
+          <Link  className='nav-link navbar-nav custom-nav-link' href="/ebike-upgrade" passHref onClick={collapseNavbar}>
             {t('eBikeUpgrade')}
           </Link>
-          <Link  className='nav-link navbar-nav' href="/about-us" passHref onClick={collapseNavbar}>
+          <Link  className='nav-link navbar-nav custom-nav-link' href="/about-us" passHref onClick={collapseNavbar}>
             {t('aboutUs')}
           </Link>
-          <Link  className='nav-link navbar-nav' href="/contact" passHref onClick={collapseNavbar}>
+          <Link  className='nav-link navbar-nav custom-nav-link' href="/contact" passHref onClick={collapseNavbar}>
             {t('contact')}
           </Link>
         </Nav>
