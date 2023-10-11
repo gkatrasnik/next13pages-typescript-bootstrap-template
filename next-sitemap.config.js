75c0eb11ec2d860bba4e559f4075319d15e5e213
Gašper Katrašnik <72498702+gkatrasnik@users.noreply.github.com>
2023-10-11 09:56:53 +0000

    exclude 404and 500 sites

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,7 @@
 module.exports = {
     siteUrl: process.env.NEXT_PUBLIC_BASE_URL,
     generateRobotsTxt: true,    
+    exclude: ['/404','/en/404', '/500', '/en/500'],
     alternateRefs: [
       {
         href: process.env.NEXT_PUBLIC_BASE_URL,
