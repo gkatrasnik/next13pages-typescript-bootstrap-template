c657722c5bc8874604c59a66f8252fc3c954dc1e
Gašper Katrašnik <gkatrasnik@gmail.com>
2023-10-11 12:41:53 +0200

    alternate refs do not work ok. all sites with langs are added either way
    alternate refs do not work ok. all sites with langs are added either way

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,16 +1,22 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: process.env.NEXT_PUBLIC_BASE_URL,
+    changefreq: 'monthly',
     generateRobotsTxt: true,    
-    exclude: ['/404','/en/404', '/500', '/en/500'],
-    alternateRefs: [
-      {
-        href: process.env.NEXT_PUBLIC_BASE_URL,
-        hreflang: 'sl',
-      },
-      {
-        href: `${process.env.NEXT_PUBLIC_BASE_URL}/en`,
-        hreflang: 'en',
-      },
-    ],
+    exclude: ['/404','/en/404', '/500', '/en/500'],    
+    additionalPaths: async (config) => {
+      const result = []
+
+      result.push({
+        loc: '/',
+        priority: 1,
+      });
+
+      result.push({
+        loc: '/en',
+        priority: 1,
+      });
+
+      return result
+    }
   };
