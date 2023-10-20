1d9f1a7ce7d46d079d108639a70e8003073b1049
Gašper Katrašnik <72498702+gkatrasnik@users.noreply.github.com>
2023-10-20 16:26:12 +0000

    test again

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -93,8 +93,7 @@ export const getStaticProps: GetStaticProps = async (context)  => {
     props: {
       messages: (await import(`../messages/${context.locale}.json`)).default,
       placesData: placesData
-    },
-    revalidate: 86400 //parseInt(process.env.GOOGLE_PLACES_DATA_REVALIDATE!) // Re-fetch the data every 24 hours (86400 seconds)
+    }
   };
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -57,8 +57,7 @@ export const getStaticProps: GetStaticProps = async (context)  => {
     props: {
       messages: (await import(`../messages/${context.locale || "sl"}.json`)).default,
       placesData: placesData
-    },
-    revalidate: 86400 //parseInt(process.env.GOOGLE_PLACES_DATA_REVALIDATE!) // Re-fetch the data every 24 hours (86400 seconds)
+    }
   };
 }
 
