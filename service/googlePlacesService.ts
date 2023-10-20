dc6bbc15a23413843c589ba03cc6f22cf8e0fcdb
Gašper Katrašnik <72498702+gkatrasnik@users.noreply.github.com>
2023-10-20 12:43:10 +0000

    fixed googlePlacesService

--- service/googlePlacesService.ts
+++ service/googlePlacesService.ts
@@ -4,5 +4,5 @@ export const fetchGooglePlacesData = async (): Promise<GooglePlaceDetails> => {
     const url = `${process.env.GOOGLE_PLACES_API_URL}?placeid=${process.env.GOOGLE_PLACES_PLACE_ID}&key=${process.env.GOOGLE_PLACES_API_KEY}`;
     const response = await fetch(url);
     const data = await response.json();
-    return data.result; 
+    return (data as GooglePlacesResponse).result; 
 } 
