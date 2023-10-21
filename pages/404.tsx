3b4b9e3ac5ae1b8574b9acbe35251c4e53d6e075
Gašper Katrašnik <72498702+gkatrasnik@users.noreply.github.com>
2023-10-21 16:57:10 +0000

    404 and 500 pages translated and noindex meta tag added

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,24 +1,35 @@
 import Image from 'next/image'
 import { GetStaticProps } from 'next';
+import { useTranslations } from 'next-intl';
+import Head from 'next/head';
 
 const Custom404 = () => {
+  const t = useTranslations('404');
+
   return (
-    <div className="error-page">
-        <Image 
-          src="/logo/logo-small.png"
-          alt="AB Bike"
-          height={150}
-          width={200}  
-          placeholder="blur"
-          blurDataURL="data:image/webp;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mOsrwcAAYMBABbFvRQAAAAASUVORK5CYII="   
-          priority    
-          className='mb-3'      
-          style={{                 
-            objectFit:"contain"                
-          }}                         
-        />
-        <h1>404 - Page Not Found</h1>
-    </div>        
+    <>
+      <Head>
+        <title>{t("title")}</title>
+        <meta name="description" content={t('description')} />
+        <meta name="robots" content="noindex"/>
+      </Head> 
+      <div className="error-page">
+          <Image 
+            src="/logo/logo-small.png"
+            alt="AB Bike"
+            height={150}
+            width={200}  
+            placeholder="blur"
+            blurDataURL="data:image/webp;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mOsrwcAAYMBABbFvRQAAAAASUVORK5CYII="   
+            priority    
+            className='mb-3'      
+            style={{                 
+              objectFit:"contain"                
+            }}                         
+          />
+          <h1>{t("description")}</h1>
+      </div> 
+    </>       
   )
 }
 
--- pages/500.tsx
+++ pages/500.tsx
@@ -1,24 +1,35 @@
 import Image from 'next/image'
 import { GetStaticProps } from 'next'
+import { useTranslations } from 'next-intl'
+import Head from 'next/head'
 
 const Custom500 = ()  => {
+  const t = useTranslations('500');
+
   return (
-    <div className="error-page">
-        <Image 
-          src="/logo/logo-small.png"
-          alt="AB Bike"
-          width={160} 
-          height={150}                   
-          placeholder="blur"
-          blurDataURL="data:image/webp;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mOsrwcAAYMBABbFvRQAAAAASUVORK5CYII="   
-          priority    
-          className='mb-3'  
-          style={{                 
-            objectFit:"contain"                
-          }}                             
-        />
-        <h1>500 - Server-side error occurred</h1>
-    </div>        
+    <>
+      <Head>
+        <title>{t("title")}</title>
+        <meta name="description" content={t('description')} />
+        <meta name="robots" content="noindex"/>
+      </Head> 
+      <div className="error-page">
+          <Image 
+            src="/logo/logo-small.png"
+            alt="AB Bike"
+            width={160} 
+            height={150}                   
+            placeholder="blur"
+            blurDataURL="data:image/webp;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mOsrwcAAYMBABbFvRQAAAAASUVORK5CYII="   
+            priority    
+            className='mb-3'  
+            style={{                 
+              objectFit:"contain"                
+            }}                             
+          />
+          <h1>{t("description")}</h1>
+      </div>  
+    </>      
   )
 }
 
